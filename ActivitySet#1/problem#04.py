d57cc8227a4795e36776298b4e1ed377dbfedf31
Shashank R <96413845+Shashank-REVA@users.noreply.github.com>
2022-05-16 10:14:31 +0000

    Python

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,3 +1,13 @@
 # Conditional Execution
 
-hrs = input("Enter hours? ")
+hrs = float(input("Enter hours? "))
+h = float(hrs)
+rate = float(input("Enter rate? "))
+r = float(rate)
+pay=hrs*rate
+if hrs>40:
+  extratime = (hrs-40)*(rate*0.5)
+  Salary = (hrs*rate) + extratime
+else:
+  Salary = hrs*rate
+print(Salary)
